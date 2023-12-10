aa44635fb1dfcec7aed788fbce31cd701e602801
vVladEr <vlader.2004.ekb@gmail.com>
2023-12-10 19:59:33 +0500

    test scapy files

--- packet_constructor.py
+++ packet_constructor.py
@@ -1,90 +1,18 @@
 import socket
 import struct
-import scapy
+from scapy.layers.inet import IP, TCP
+from scapy.sendrecv import sr1, srp1
 
 SRC_IP = socket.gethostbyname(socket.gethostname())
-SRC_PORT = 80
+SRC_PORT = 1030
 
 
-def calculate_checksum(data):
-    s = 0
-    if len(data) % 2:
-        data += b'0'
-    for i in range(0, len(data), 2):
-        s += data[i + 1] + (data[i] << 8)
-    while s >> 16:
-        s = (s & 0xFFFF) + (s >> 16)
-    s = ~s & 0xffff
-    return s
+def syn_scan(ip, port):  # В данном месте проводится сканирование путем отправки пакетов
+    syn_packet = IP(dst=ip) / TCP(dport=port, flags="S")  # Флаг S означает SYN пакет
+    resp_packet = srp1(syn_packet, timeout=2)  # Время ожидания пакета можно ставить свое
+    if resp_packet is not None:
+        if resp_packet.getlayer('TCP').flags & 0x12 != 0:
+            print(f"{ip}:{port} is open/{resp_packet.sprintf('%TCP.sport%')}")
 
 
-def _parse_ipaddr_to_octets(ipaddr: str):
-    return list(map(int, ipaddr.split(".")))
-
-
-def _create_ip_header(dest_ip: str):
-    ip_version = 4
-    header_length = 5
-    tos = 0
-    total_length = 40  # ip_header + tcp_header
-    id = 12345
-    flags = 0
-    frag_off = 0
-    ttl = 255
-    protocol = socket.IPPROTO_TCP
-    cksum = 0
-    saddr = socket.inet_aton(SRC_IP)
-    daddr = socket.inet_aton(dest_ip)
-    first_pack = struct.pack('!BBHHHBBH4s4s', (ip_version << 4) + header_length,
-                             tos, total_length, id, (flags << 13) + frag_off, ttl, protocol, cksum,
-                             saddr, daddr)
-    cksum = calculate_checksum(first_pack)
-    return struct.pack('!BBHHHBBH4s4s', (ip_version << 4) + header_length,
-                       tos, total_length, id, (flags << 13) + frag_off, ttl, protocol, cksum,
-                       saddr, daddr)
-
-
-def _create_tcp_header(dest_port: int, dest_ip: str):
-    seq_num = 0
-    ack_num = 0
-    hl = 5
-    syn = 1
-    win_size = socket.htons(5840)
-    chksum = 0
-    urgp = 0
-    flag_packet = syn << 1
-    hl_reserve = hl << 4
-    tcp_header = struct.pack("HHLLBBHHH", SRC_PORT, dest_port,
-                             seq_num, ack_num,
-                             hl_reserve, flag_packet, win_size,
-                             chksum, urgp)
-    ps_header = _create_pseudo_tcp_header(dest_ip, len_tcp_header=len(tcp_header))
-    ps_header = tcp_header + ps_header
-    chksum = calculate_checksum(ps_header)
-    return struct.pack("HHLLBBHHH", SRC_PORT, dest_port,
-                       seq_num, ack_num,
-                       hl_reserve, flag_packet, win_size,
-                       chksum, urgp)
-
-
-def _create_pseudo_tcp_header(dest_ip: str, len_tcp_header: int):
-    saddr = socket.inet_aton(SRC_IP)
-    daddr = socket.inet_aton(dest_ip)
-    zeros = 0
-    proto = socket.IPPROTO_TCP
-    return struct.pack("!4s4sBBH", saddr, daddr, zeros, proto, len_tcp_header)
-
-
-def create_packet(dest_ip: str, dest_port: str):
-    ip_header = _create_ip_header(dest_ip)
-    tcp_header = _create_tcp_header(int(dest_port), dest_ip)
-    return ip_header + tcp_header
-
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((SRC_IP, SRC_PORT))
-print("------------")
-dest_ip = "1.1.1.1"
-packet = create_packet(dest_ip, "80")
-s.sendto(packet, (dest_ip, 80))
-print("end")
+syn_scan("1.1.1.1", 80)
---
+++ test_file.py
@@ -0,0 +1,40 @@
+import random
+
+
+# Define end host and TCP port range
+from scapy.layers.inet import IP, TCP, ICMP
+from scapy.sendrecv import sr1, sr, srp1
+
+host = "1.1.1.1"
+port_range = [22, 23, 80, 443, 3389]
+
+# Send SYN with random Src Port for each Dst port
+for dst_port in port_range:
+    src_port = random.randint(1025, 65534)
+    resp = srp1(
+        IP(dst=host)/TCP(sport=src_port,dport=dst_port,flags="S"),timeout=1,
+        verbose=0,
+    )
+
+    if resp is None:
+        print(f"{host}:{dst_port} is filtered (silently dropped).")
+
+    elif(resp.haslayer(TCP)):
+        if(resp.getlayer(TCP).flags == 0x12):
+            # Send a gratuitous RST to close the connection
+            send_rst = sr(
+                IP(dst=host)/TCP(sport=src_port,dport=dst_port,flags='R'),
+                timeout=1,
+                verbose=0,
+            )
+            print(f"{host}:{dst_port} is open.")
+
+        elif (resp.getlayer(TCP).flags == 0x14):
+            print(f"{host}:{dst_port} is closed.")
+
+    elif(resp.haslayer(ICMP)):
+        if(
+            int(resp.getlayer(ICMP).type) == 3 and
+            int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]
+        ):
+            print(f"{host}:{dst_port} is filtered (silently dropped).")
